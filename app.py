b2ed6ce209d63441e7f4290a5fe50d6a8b81bfb7
MalMWare <malkelleymartin@gmail.com>
2023-06-02 22:21:35 -0700

    started application factory

--- app.py
+++ app.py
@@ -1,10 +1,2 @@
-#config
-from flask import Flask
-app = Flask(__name__)
-
-#index route
-@app.route('/')
-def index():
-    return "Hello, this is Ball Corp!"
-
-#
+from ball import create_app
+app = create_app()
